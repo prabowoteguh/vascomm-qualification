bf0adeaedbd0d430f1700be4cfcf96439e434db6
ilhamfathur27 <ilhammrohang@gmail.com>
2021-05-19 14:44:48 +0700

    nambah

---
+++ app/Http/Controllers/ContohController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class ContohController extends Controller
+{
+    
+    //
+    function index(){
+        $client = new \GuzzleHttp\Client(['base_uri' => 'https://smksumatra40.sch.id/']);
+        $response = $client->request('GET', '/api/aktivitas.html?limit=3', array("content-type" => "application/x-www-form-urlencoded"))->getBody()->getContents();
+        $response = json_decode($response);
+        $response = $response->result->datas;
+        
+        return view("brader", ["data" => $response]);
+    }
+    function data(){
+
+
+        $data[] = array(
+            "status" => true,
+            "message" => "Sukses",
+            "data" => "base64",
+        );
+        $data[] = array(
+            "kode_aktivitas" => "AKTVS7668010529202108116921",
+            "judul" => "Menghapus File",
+            "user" => "1 - resitdc - Restu Dwi Cahyo",
+            "waktu" => "Selasa, 11 Mei 2021 - 08:01:29",
+            "keterangan" => "resitdcmenghapus file dengan nama f28a7e159347a5598ff11b4a0f307cf7.apk dengan tipe application melalui ip 114.5.218.253 dengan browser Chrome pada OS Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+        );
+        $data[] = array(
+            "kode_aktivitas" => "AKTVS8477010503202108112004",
+            "judul" => "Login ke dashboard",
+            "user" => "1 - resitdc - Restu Dwi Cahyo",
+            "waktu" => "Selasa, 11 Mei 2021 - 08:01:03",
+            "keterangan" => "Restu Dwi Cahyo login ke dashboard melalui ip 114.5.218.253 dengan browser Chrome pada OS Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+        );
+
+        return json_encode($data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContohController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/data', [ContohController::class, 'data']);
+Route::get('/testing', [ContohController::class, 'index']);
+
